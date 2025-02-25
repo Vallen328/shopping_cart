eba430264b096c7fa66b4ce1fbde67c8bea044ea
Vallen Dsouza <123362088+Vallen328@users.noreply.github.com>
2025-02-25 13:12:21 +0530

    update

--- shopping_cart_route/src/App.jsx
+++ shopping_cart_route/src/App.jsx
@@ -3,13 +3,13 @@ import Cart from "./Pages/Cart";
 import Home from "./Pages/Home";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 
 import ProtectedRoutes from "./Services/ProtectedRoutes";
 const App = () => {
-  const { user, loginWithRedirect } = useAuth0();
+  //const { user, loginWithRedirect } = useAuth0();
 
-  console.log("Current user", user)
+  // console.log("Current user", user)
   return (
     <>
       <Routes>
--- shopping_cart_route/src/main.jsx
+++ shopping_cart_route/src/main.jsx
@@ -6,22 +6,22 @@ import "bootstrap/dist/js/bootstrap.bundle.js";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
-import { Auth0Provider } from '@auth0/auth0-react';
+// import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Auth0Provider 
+    {/* <Auth0Provider 
       domain="dev-b486jeq6s6wjswrd.us.auth0.com"
       clientId="LanXQYynj62VH8yJRaigccTl1dZsvz6u"
       authorizationParams={{
       redirect_uri: window.location.origin
-    }}>
+    }}> */}
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-    </Auth0Provider>
+    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
